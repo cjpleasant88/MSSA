aabbe0b3b3790122415b37eec9df6a10df491c8e
Caleb Pleasant <calebpleasant@gmail.com>
2020-03-28 17:54:39 -0700

    Can now convert numbers up to int MaxValue

--- ISTA_421_EX_1E_BaseNumberConversion/Program.cs
+++ ISTA_421_EX_1E_BaseNumberConversion/Program.cs
@@ -12,13 +12,24 @@ namespace ISTA_421_EX_1E_BaseNumberConversion
             Console.Write("Please enter the integer to convert: ");
             string n1 = Console.ReadLine();
             int number = int.Parse(n1);
-
+            
             Console.Write("Please enter the base to convert from [2 | 8 | 10]: ");
             string n2 = Console.ReadLine();
             int from = int.Parse(n2);
 
-            Console.WriteLine($"Number: {number}, base:{from}");
-            int result = 0;
+            Console.WriteLine($"Number: {number}, base: {from}");
+            //changing result from int to string. largest int that can be converted is 1023 
+            //when 1023 decimal converted to binary ends up 1111111111
+            //1024 decimal to binary is 10000000000 and is too large for int type to hold--> exception is thrown
+            //int result = 0;
+
+            //trying to input 2147483648 base 10 results in stack overflow.
+            //can now convert numbers all the way to:
+            //Max decimal value = int MaxValue: 2147483647
+            //Max binary value  = 1111111111111111111111111111111
+            //Max Octal value   = 17777777777
+            string result = "";
+
             if (from == 10)
             {
                 result = Util.dec2bin(number);
@@ -50,18 +61,18 @@ namespace ISTA_421_EX_1E_BaseNumberConversion
         public class Util
         {
             //Converts Decimal to Binary
-            public static int dec2bin(int number)
+            public static string dec2bin(int number)
             {
                 return dec2base(number, 2);
             }
             //Converts Decimal to Octal
-            public static int dec2oct(int number)
+            public static string dec2oct(int number)
             {
                 return dec2base(number, 8);
             }
 
             //Converts Decimal number to any given base
-            public static int dec2base(int number, int toBase)
+            public static string dec2base(int number, int toBase)
             {
                 string reverseResult = "";
                 while (number > 0)
@@ -73,34 +84,31 @@ namespace ISTA_421_EX_1E_BaseNumberConversion
             }
 
             //Converts Binary to Decial
-            public static int bin2dec(int number)
+            public static string bin2dec(int number)
             {
                 return base2dec(number, 2);
             }
 
             //Converts Binary to Octal (binary to decimal and then the decimal to octal)
-            public static int bin2oct(int number)
+            public static string bin2oct(int number)
             {
-                int result = dec2oct(bin2dec(number));
-                return result;
+                return dec2oct(Convert.ToInt32(bin2dec(number)));
             }
 
             //Converts Octal to Deciaml
-            public static int oct2dec(int number)
+            public static string oct2dec(int number)
             {
                 return base2dec(number, 8);
             }
 
             //Converts Octal to Binary (octal to decimal and then the decimal to binary)
-            public static int oct2bin(int number)
+            public static string oct2bin(int number)
             {
-                int result = dec2bin(oct2dec(number));
-
-                return result;
+                return dec2bin(Convert.ToInt32(oct2dec(number)));
             }
 
             //Converts a given base number to Decimal value
-            public static int base2dec(int number, int fromBase)
+            public static string base2dec(int number, int fromBase)
             {
                 string input = Convert.ToString(number);
                 int result = 0;
@@ -108,18 +116,19 @@ namespace ISTA_421_EX_1E_BaseNumberConversion
                 {
                     result += Convert.ToInt32(input[input.Length - 1 - i].ToString()) * (int)Math.Pow(fromBase, i);
                 }
-                return result;
+                return result.ToString();
             }
 
             //Reverses a given string value
-            public static int ReverseString(string input)
+            public static string ReverseString(string input)
             {
                 string result = "";
                 for (int i = input.Length - 1; i >= 0; i--)
                 {
                     result += input[i];
                 }
-                return Convert.ToInt32(result);
+                //return Convert.ToInt32(result);
+                return result;
             }
         }
     }
